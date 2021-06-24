9b79617fab4c2f195c37e60d1b8f428dfb39a1a0
Nashit Meraan <Nashit Meraan@DESKTOP-KNMG9OO>
2021-06-25 01:37:16 +0530

    For Automation Project

---
+++ E2EJenkins/src/testcases/TC_003.java
@@ -0,0 +1,25 @@
+package testcases;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_003 {
+	@Test
+	public static void TestCase()
+	{
+		System.setProperty("webdriver.chrome.driver","./Driver/chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.facebook.com/");
+		driver.manage().timeouts().pageLoadTimeout(100, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.findElement(By.xpath("//a[text()=\"English (UK)\"]")).click();;
+		driver.findElement(By.xpath("//input[@id=\"email\"]")).sendKeys("Hello");;
+		driver.findElement(By.xpath("//input[@id=\"pass\"]")).sendKeys("Hellopass");
+		driver.quit();
+	}
+}
